99fc8feafb6ae61899643b43d74f3f4bc512b28a
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-11 23:54:09 +0300

    jpa auditing implementation

---
+++ src/test/java/Wanjohi/Lewis/Security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Wanjohi.Lewis.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

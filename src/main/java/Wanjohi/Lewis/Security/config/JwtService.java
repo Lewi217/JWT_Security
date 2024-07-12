6c148482155ab0c85bd871fb053fe653581ea556
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-12 22:47:34 +0300

    jpa auditing implementation

--- src/main/java/Wanjohi/Lewis/Security/config/JwtService.java
+++ src/main/java/Wanjohi/Lewis/Security/config/JwtService.java
@@ -2,12 +2,15 @@ package Wanjohi.Lewis.Security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -26,11 +29,33 @@ public class JwtService<userDetails> {
         return claimsResolver.apply(claims);
     }
 
-   public String generateToken(
-           Map<String, Object> extraClaims
-           User
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
-   )
+   public String generateToken(
+           Map<String, Object> extraClaims,
+           UserDetails userDetails
+   ){
+     return Jwts
+             .builder()
+             .setClaims(extraClaims)
+             .setSubject(userDetails.getUsername())
+             .setIssuedAt(new Date(System.currentTimeMillis()))
+             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+             .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+             .compact();
+   }
+
+   public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return(username.equals(userDetails.getUsername())) && !isTokenValid(token);
+   }
+
+    private boolean isTokenValid(String token) {
+        return false;
+    }
+
 
     private Claims extractAllClaims(String token){
         return Jwts
